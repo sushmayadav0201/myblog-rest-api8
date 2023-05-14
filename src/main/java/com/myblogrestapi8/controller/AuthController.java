a96fd6b849371aadb7288ea2b4643392b502aeb4
Sushma Kumari <sushmayadav0201@gmail.com>
2023-05-14 17:00:30 +0530

    developing signup feature Rest Api working

--- src/main/java/com/myblogrestapi8/controller/AuthController.java
+++ src/main/java/com/myblogrestapi8/controller/AuthController.java
@@ -1,6 +1,11 @@
 package com.myblogrestapi8.controller;
 
+import com.myblogrestapi8.entity.Role;
+import com.myblogrestapi8.entity.User;
 import com.myblogrestapi8.payload.LoginDto;
+import com.myblogrestapi8.payload.SignUpDto;
+import com.myblogrestapi8.repository.RoleRepository;
+import com.myblogrestapi8.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -8,17 +13,29 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Collections;
+
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private RoleRepository roleRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     @Autowired
     private AuthenticationManager authenticationManager;
+
     //http://localhost:8080/api/auth/signin
     @PostMapping("/signin")
     public ResponseEntity<String> authenticateUser(@RequestBody LoginDto loginDto) {
@@ -28,5 +45,30 @@ public class AuthController {
         return new ResponseEntity<>("User signed-in successfully!.",
                 HttpStatus.OK);
     }
+
+    @PostMapping("/signup")
+    public ResponseEntity<?> registerUser(@RequestBody SignUpDto signUpDto) {
+        if(userRepository.existsByUsername(signUpDto.getUsername())){
+            return new ResponseEntity<>("Username is already taken!", HttpStatus.BAD_REQUEST);
+    }
+    if(userRepository.existsByEmail(signUpDto.getEmail())) {
+        return new ResponseEntity<>("Email is already taken!",
+                HttpStatus.BAD_REQUEST);
+
+    }
+
+    //create user object
+    User user = new User();
+    user.setName(signUpDto.getName());
+    user.setUsername(signUpDto.getUsername());
+    user.setEmail(signUpDto.getEmail());
+    user.setPassword(signUpDto.getPassword());
+
+    Role roles = roleRepository.findByName("ROLE_ADMIN").get();
+    user.setRoles(Collections.singleton(roles));
+    userRepository.save(user);
+
+    return new ResponseEntity("User registered successfully",HttpStatus.OK);
+}
     }
 
--- src/main/java/com/myblogrestapi8/repository/UserRepository.java
+++ src/main/java/com/myblogrestapi8/repository/UserRepository.java
@@ -10,5 +10,6 @@ public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByUsernameOrEmail(String username, String email);
     Optional<User> findByUsername(String username);
     Boolean existsByUsername(String username);
+
     Boolean existsByEmail(String email);
 }
